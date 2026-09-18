e1243d9b908d123f2c893a28780152731f2d4ce8
Moeinemami <moein.emami2005@gmail.com>
2026-09-18 04:29:01 +0330

    Create L1_moghayese.py

---
+++ 1st session/L1_moghayese.py
@@ -0,0 +1,30 @@
+
+"""
+
+@author: MOEIN
+
+"""
+
+#---------------------------------
+
+
+a = 10
+b = 43
+c = 27
+d = 15
+ 
+print(a==b) #False
+print(c==d) #False
+print(a!=d) #True
+print(c!=b) #True
+print(c>d)  #True
+print(a>b)  #False
+print(c>=b) #False
+print(a>=d) #False
+print(a<c)  #True
+print(d<b)  #True
+print(c<=b) #True
+print(b<=a) #False
+  
+#----------------------------------
+
