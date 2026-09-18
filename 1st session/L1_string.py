5bb4e733647adee8f30a2d9b1ce8259a409018a2
Moeinemami <moein.emami2005@gmail.com>
2026-09-18 04:29:09 +0330

    Create L1_string.py

---
+++ 1st session/L1_string.py
@@ -0,0 +1,36 @@
+"""
+@author: MOEIN
+"""
+
+a = '10'
+b ="864168"
+c = 'moein'
+d = '231.4123569'
+e = 'we are learning python'
+
+print(a) #10
+print(b) #864168
+print(c) #moein
+print(d) #3231.4123569
+print(e) #we are learning python
+
+#----------------------------
+
+print(type(a)) #<class 'str'>
+print(type(b)) #<class 'str'>
+print(type(c)) #<class 'str'>
+print(type(d)) #<class 'str'>
+print(type(e)) #<class 'str'>
+
+#-----------------------------
+
+print(len(a)) #2
+print(len(b)) #6
+print(len(c)) #5
+print(len(d)) #11
+print(len(e)) #22
+
+#-----------------------------
+
+
+
